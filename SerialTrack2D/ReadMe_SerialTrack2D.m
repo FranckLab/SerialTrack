8ebe33d662ffa33b14889b4fdce34a8161650de2
JIN YANG <30322736+jyang526843@users.noreply.github.com>
2022-07-05 09:49:39 -0500

    Add files via upload

--- SerialTrack2D/ReadMe_SerialTrack2D.m
+++ SerialTrack2D/ReadMe_SerialTrack2D.m
@@ -2,4 +2,6 @@
 1. Stay on this path;
 2. Open an example of the main file from the subfolder "./Example_main_files/";
 3. Prepare your images;
-4. Execute main file(s) and enjoy it!
+4. Execute the main file(s) and enjoy it!
+
+

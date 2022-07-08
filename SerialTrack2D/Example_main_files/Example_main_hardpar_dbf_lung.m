c0e6386cbb1cd1fb22291dff171edf5617183bfe
Alex Landauer <alex.landauer@gmail.com>
2022-07-08 16:58:02 -0400

    Update Example_main_hardpar_dbf_lung.m

--- SerialTrack2D/Example_main_files/Example_main_hardpar_dbf_lung.m
+++ SerialTrack2D/Example_main_files/Example_main_hardpar_dbf_lung.m
@@ -59,23 +59,23 @@ end
 %%%%% Particle detection parameters %%%%%
 %%%%% Bead Parameter %%%%%
 BeadPara.thres = 0.5;           % Threshold for detecting particles
-BeadPara.beadSize = 3;          % Estimated radius of a single particle
-BeadPara.minSize = 2;           % Minimum radius of a single particle
-BeadPara.maxSize = 200;         % Maximum radius of a single particle
-BeadPara.winSize = [5, 5];      % By default
-BeadPara.dccd = [1,1];          % By default
-BeadPara.abc = [1,1];           % By default
-BeadPara.forloop = 1;           % By default
-BeadPara.randNoise = 1e-7;      % By default
+BeadPara.beadSize = 3;          % Estimated radius of a single particle [px]
+BeadPara.minSize = 2;           % Minimum radius of a single particle [px^2]
+BeadPara.maxSize = 200;         % Maximum radius of a single particle [px^2]
+BeadPara.winSize = [5, 5];      % Default [not used in 2D]
+BeadPara.dccd = [1,1];          % Default [not used in 2D]
+BeadPara.abc = [1,1];           % Default [not used in 2D]
+BeadPara.forloop = 1;           % Default [not used in 2D]
+BeadPara.randNoise = 1e-7;      % Default [not used in 2D]
 BeadPara.PSF = [];              % PSF function; Example: PSF = fspecial('disk', BeadPara.beadSize-1 ); % Disk blur
-BeadPara.distMissing = 2;       % Distance threshold to check whether particle has a match or not 
+BeadPara.distMissing = 2;       % Distance threshold to check whether particle has a match or not [px]
 BeadPara.color = 'white';       % Bead color: 'white' -or- 'black'
 
 
 %% SerialTrack particle tracking
 
 %%%%% Multiple particle tracking (MPT) Parameter %%%%%
-MPTPara.f_o_s = 30;              % Size of search field: max(|u|,|v|)
+MPTPara.f_o_s = 30;              % Size of search field: max(|u|,|v|) [px]
 MPTPara.n_neighborsMax = 25;     % Max # of neighboring particles
 MPTPara.n_neighborsMin = 25;     % Min # of neighboring particles
 MPTPara.locSolver = 2;           % Local solver: 1-topology-based feature; 2-histogram-based feature first and then topology-based feature;
@@ -85,15 +85,15 @@ MPTPara.outlrThres = 2;          % Threshold for removing outliers in TPT
 MPTPara.maxIterNum = 20;         % Max ADMM iteration number
 MPTPara.iterStopThres = 1e-2;    % ADMM iteration stopping threshold
 MPTPara.strain_n_neighbors = 20; % # of neighboring particles used in strain gauge
-MPTPara.strain_f_o_s = 60;       % Size of virtual strain gauge
+MPTPara.strain_f_o_s = 60;       % Size of virtual strain gauge [px]
 MPTPara.usePrevResults = 0;      % Whether use previous results or not: 0-no; 1-yes;  
 
 
 %%%% Postprocessing: merge trajectory segments %%%%%
-distThres = 1; % distance threshold to connect split trajectory segments
+distThres = 1; % distance threshold to connect split trajectory segments [px]
 extrapMethod = 'pchip';  % extrapolation scheme to connect split trajectory segments
                          % suggestion: 'nearest' for Brownian motion                          
-minTrajSegLength = 10;    % the minimum length of trajectory segment that will be extrapolated 
+minTrajSegLength = 10;    % the minimum length of trajectory segment that will be extrapolated [px]
 maxGapTrajSeqLength = 0; % the max frame# gap between connected trajectory segments
 
 
